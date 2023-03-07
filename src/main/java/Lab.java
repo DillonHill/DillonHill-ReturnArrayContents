0ed2704357c8dd370e6e7705e69302ac5535c05a
Dillon Hill <tuj66029@temple.edu>
2023-03-07 23:09:40 +0000

    fixed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -32,7 +32,7 @@ public class Lab {
     public String arrayToString(int[] nums){
         String retval = "";
 
-        if(int i = 0; i < nums.Length; i++){
+        for(int i = 0; i < nums.length; i++){
             retval = retval + nums[i] + " ";
         }
 
