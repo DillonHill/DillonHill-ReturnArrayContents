462d6bb37c969fff44967becb84a582369804fd3
Dillon Hill <tuj66029@temple.edu>
2023-03-07 23:08:54 +0000

    Return Array Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String retval = "";
+
+        if(int i = 0; i < nums.Length; i++){
+            retval = retval + nums[i] + " ";
+        }
+
+        return retval;
     }
 }
